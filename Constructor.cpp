88a2bff03236cd1731b3b11a823efa6278d29b21
Atharvvvvvv <jadonatharv1@gmail.com>
2025-04-06 22:34:17 +0530

    new files

--- Constructor.cpp
+++ Constructor.cpp
@@ -2,29 +2,48 @@
 #include <string>
 
 using namespace std;
-class Account{
+
+class Account {
 private:
-    double balance;
-
-
-public:
-    Account(string accountid , string username){
-        this->accountid = accountid;
-        this->username = username;
-        
-    }
-    void getinfo(){
-        cout<<"This is  your account id :"<<accountid<<endl;
-        cout<<"This is your username :"<<username<<endl;
-
-    }
-
     string accountid;
     string username;
+    double balance;
+
+public:
+    // Constructor with initializer list
+    Account(const string& accountid, const string& username) : accountid(accountid), username(username), balance(0.0) {
+        if (accountid.empty() || username.empty()) {
+            throw invalid_argument("Account ID and username cannot be empty.");
+        }
+    }
+
+    // Getter methods
+    string getAccountId() const {
+        return accountid;
+    }
+
+    string getUsername() const {
+        return username;
+    }
+
+    double getBalance() const {
+        return balance;
+    }
+
+    void getinfo() const {
+        cout << "This is your account ID: " << accountid << endl;
+        cout << "This is your username: " << username << endl;
+        cout << "Your current balance is: " << balance << endl;
+    }
 };
-int main(){
-    Account a1("abcd", "Atharv");
-    a1.getinfo();
+
+int main() {
+    try {
+        Account a1("abcd", "Atharv");
+        a1.getinfo();
+    } catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+    }
 
     return 0;
 }
---
+++ RecordManager.c++
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+class Strudent{
+public:
+    string name;
+    int roll;
+    int marks;
+};
